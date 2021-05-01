d690f6564f1bb03b8ac46dcada1f464d02702e9d
Mehdi Moradi Azad <mehdi.moradi@snapp.cab>
2021-05-02 00:13:58 +0430

    Added flask restful package

--- authz/__init__.py
+++ authz/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_restful import Api
 from authz.config import Config
+
+api = Api()
+
+from authz import resource
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  # Load configs from ENV variables
+    api.init_app(app)
     return app
