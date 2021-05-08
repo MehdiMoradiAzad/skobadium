c7aced06653c9b05e9ff3edc3fd7b16753ee011e
Mehdi Moradi Azad <mehdi.moradi@snapp.cab>
2021-05-08 16:51:54 +0430

    Added flask migrate and install mysql driver

--- authz/__init__.py
+++ authz/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
 from authz.config import Config
+
+db = SQLAlchemy()
+mg = Migrate()
 
 api = Api()
 
@@ -9,5 +15,7 @@ from authz import resource
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  # Load configs from ENV variables
+    db.init_app(app)
+    mg.init_app(app, db)
     api.init_app(app)
     return app
