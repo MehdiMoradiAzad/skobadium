c10c832d86c463ee0a9342b5bda3e06b5c95f75c
Mehdi Moradi Azad <mehdi.moradi@snapp.cab>
2021-05-09 10:42:59 +0430

    Added all user.py files

--- authz/config/config.py
+++ authz/config/config.py
@@ -2,9 +2,22 @@ from os import environ
 
 class Config:
 
+    ##### Global Configuration #####
+
     ENV = environ.get("SKOB_AUTHZ_ENV", "production")
 
     DEBUG = int(environ.get("SKOB_AUTHZ_DEBUG", "0"))
 
     TESTING = int(environ.get("SKOB_AUTHZ_TESTING", "0"))
 
+    ##### Database Configuration #####
+
+    SQLALCHEMY_DATABASE_URI = environ.get("SKOB_AUTHZ_DATABASE_URI", None)
+
+    SQLALCHEMY_TRACK_MODIFICATION = TESTING
+
+    ##### User Configuration #####
+
+    USER_DEFAULT_ROLE = environ.get("SKOB_AUTHZ_USER_DEFAULT_ROLE", "member")
+
+    USER_DEFAULT_STATUS = environ.get("SKOB_AUTHZ_USER_DEFAULT_STATUS", "inactive")
