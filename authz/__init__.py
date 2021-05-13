d3a90e2a875526c98ac17a67b63ea5fd4131165f
Mehdi Moradi Azad <mehdi.moradi@snapp.cab>
2021-05-13 19:07:01 +0430

    Adding flask marshmallow

--- authz/__init__.py
+++ authz/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
@@ -6,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from authz.config import Config
 
 db = SQLAlchemy()
+ma = Marshmallow()
 mg = Migrate()
 
 api = Api()
@@ -16,6 +18,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  # Load configs from ENV variables
     db.init_app(app)
+    ma.init_app(app)
     mg.init_app(app, db)
     api.init_app(app)
     return app
