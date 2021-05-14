8ad4c1cd1d212704df07840bb8a131b705225911
Mehdi Moradi Azad <mehdi.moradi@snapp.cab>
2021-05-14 23:22:13 +0430

    Added user controller

--- authz/controller/user.py
+++ authz/controller/user.py
@@ -1,36 +1,98 @@
 from flask import abort, request
 from authz import db
 from authz.model import User
+from authz.schema import UserSchema
 
 class UserController:
 
     def create_user():
         if request.content_type != "application/json":
-            abort(415)
-        data = request.get_json()
-        if len(data) != 2:
-            abort(400)
-        if "username" not in data or "password" not in data:
-            abort(400)
+            abort(415) # Bad media type.
+        user_schema = UserSchema(only=["username", "password"])
+        try:
+            data = user_schema.load(request.get_json())  # Validate request data.
+        except:
+            abort(400) # Invalid request.
         if not data["username"] or not data["password"]:
-            abort(400)
-        if type(data["username"]) is not str or type(data["password"]) is not str:
-            abort(400)
-        user = User.query.filter_by(username=data["username"]).first()
+            abort(400) # Empty data.
+        try:
+            user = User.query.filter_by(username=data["username"]).first() # Select a user.
+        except:
+            abort(500) # Databse error.
         if user is not None:
-            abort(409)
-        user = User(username=data["username"], password=data["password"])
-        db.session.add(user)
-        db.session.commit()
+            abort(409) # User is already registered.
+        user = User(username=data["username"], password=data["password"]) # Create new user.
+        db.session.add(user) # Add to databse session.
+        try:
+            db.session.commit() # Database CREATE query.
+        except:
+            db.session.rollback()
+            abort(500) # Database error.
+        user_schema = UserSchema()
+        return {
+            "user": user_schema.dump(user)
+        }, 201
 
     def get_users():
-        pass
+        try:
+            users = User.query.all()
+        except:
+            abort(500) # Database error.
+        users_schema = UserSchema(many=True)
+        return {
+            "users": users_schema.dump(users)
+        }, 200
 
     def get_user(user_id):
-        pass
+        try:
+            user = User.query.get(user_id)
+        except:
+            abort(500) # Database error.
+        if user is None:
+            abort(404)
+        user_schema = UserSchema()
+        return {
+            "user": user_schema.dump(user)
+        }, 200
 
     def update_user(user_id):
-        pass
+        if request.content_type != "application/json":
+            abort(415)
+        user_schema = UserSchema(only=["password"])
+        try:
+            data = user_schema.load(request.get_json()) # Validate request data.
+        except:
+            abort(400)
+        if not data["password"]:
+            abort(400)
+        try:
+            user = User.query.get(user_id) # Selet the user.
+        except:
+            abort(500) # Database error.
+        if user is None:
+            abort(404)
+        user.password = data["password"]
+        try:
+            db.session.commit() # Database UPDATE query.
+        except:
+            db.session.rollback()
+            abort(500) # Database error.
+        user_schema = UserSchema()
+        return {
+            "user": user_schema.dump(user)
+        }, 200
 
     def delete_user(user_id):
-        pass
+        try:
+            user = User.query.get(user_id) # Selet the user.
+        except:
+            abort(500) # Database error.
+        if user is None:
+            abort(404)
+        db.session.delete(user)
+        try:
+            db.session.commit() # Database DELETE query.
+        except:
+            db.session.rollback()
+            abort(500) # Database error.
+        return {}, 204
